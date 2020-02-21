6eb515b57e21821a9a70a99dfe26e5b6d286eed9
Matt Pugh <matthew.o.pugh@gmail.com>
2020-02-20 22:49:29 -0800

    Updating with exercise 4.2

--- chapter4/my_atof.c
+++ chapter4/my_atof.c
@@ -30,7 +30,8 @@ int my_getline(char s[], int lim)
 
 double my_atof(char s[]) {
     double val, power;
-    int i, sign;
+    int i, sign, exponent, exponent_sign;
+    long exp;
 
     for (i = 0; isspace(s[i]); i++) // Skip whitespace
         ;
@@ -45,5 +46,19 @@ double my_atof(char s[]) {
         val = 10.0 * val + (s[i] - '0');
         power *= 10.0;
     }
-    return sign * val / power;
+    if (s[i] == 'e' || s[i] == 'E')
+        i++;
+    exponent_sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (exponent = 0; isdigit(s[i]); i++) 
+        exponent = exponent * 10 + (s[i] - '0');
+    // printf("Exponent: %d\n", exponent);
+    for (exp = 1, i = 0; i < exponent; i++, exp *= 10)
+        ;
+    // printf("Exponent expanded: %ld\n", exp);
+    if (exponent_sign == -1) 
+        return (sign * val / power) / exp; 
+    else
+        return (sign * val / power) * exp;
 }
